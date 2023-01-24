dbd88a231842881e7c85f93d368b9885d730463c
abdalle-github <121129777+abdalle-github@users.noreply.github.com>
2023-01-24 16:25:49 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+
+let homeEl = document.getElementById("home-el")
+
+let home = 0
+
+function add1PointH() {
+    home += 1
+    homeEl.textContent = home
+}
+
+function add2PointH() {
+    home += 2
+    homeEl.textContent = home
+}
+
+function add3PointH() {
+    home += 3
+    homeEl.textContent = home
+}
+
+
+
+
+
+let guestEl = document.getElementById("guest-el")
+
+let guest = 0
+
+function add1PointG() {
+    guest += 1
+    guestEl.textContent = guest
+}
+
+function add2PointG() {
+    guest += 2
+    guestEl.textContent = guest
+}
+
+function add3PointG() {
+    guest += 3
+    guestEl.textContent = guest
+}
